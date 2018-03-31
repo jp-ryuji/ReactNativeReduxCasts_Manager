d530903cb9c9d0cea635373b7175b505ca8b136a
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-03-31 18:23:04 +0900

    Login form with spinner and error (using redux thunk)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
 
@@ -21,10 +21,12 @@ class App extends Component {
   }
 
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
     return (
       // NOTE: The provider tag is what connects to all those different connect tags that we scatter about our application.
       // And the provider tag makes sure that all those connect tags can get access to the store, grab out redux state and then pass it off to the individual components.
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <LoginForm />
       </Provider>
     );
